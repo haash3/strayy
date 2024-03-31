42c7c466d5b5a1731acef49d71badf225d6bd2a1
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-03-31 20:46:07 +0530

    added Featured section

--- stray/src/pages/home/Home.jsx
+++ stray/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Hero} from './components/Hero'
+import { Featured } from './components/Featured'
 
 export const Home = () => {
   return (
     <>
     <Hero />
-    
+    <Featured />
     </>
   )
 }
