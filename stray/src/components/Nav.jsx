66690fd4fd1cc9a58e92656aad35ff1a7cdd754b
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-03-31 14:57:04 +0530

    created links to navbar

--- stray/src/components/Nav.jsx
+++ stray/src/components/Nav.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import { Cart } from '@phosphor-icons/react';
 import { ShoppingBag } from "@phosphor-icons/react";
-
 
 // Main-Code
 export const Nav = () => {
   return (
-    <nav 
-    className="flex items-center
-               justify-between padding-x py-2">
-      <h4
-        className="font-poppins font-bold text-4xl italic"
+    <nav
+      className="flex items-center
+               justify-between padding-x py-2"
+    >
+      <Link
+        className="font-poppins font-bold
+                   text-4xl italic"
+        to="/home"
       >
-        stray. </h4>
+        stray.
+      </Link>
       <div
         className="flex gap-8 pr-4 
                   font-robo uppercase
                   tracking-tight text-lg items-center"
       >
-        <Link>Shop</Link>
-        <Link className="new-arrivals">New</Link>
-        <Link>Help</Link>
-        <Link
-         className="flex gap-1">
-          Cart<ShoppingBag size={28} />
-        </Link>
+        <Link to="/shop">Shop</Link>
+        <Link to="/new" className="new-arrivals">New</Link>
+        <Link to="/help">Help</Link>
+        <Link to="/cart" className="flex gap-1">Cart<ShoppingBag size={28} /></Link>
       </div>
     </nav>
   );
