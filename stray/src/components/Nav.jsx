0d5869d9cb3416cec81bd42d50012a72431de4c1
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-03-31 10:17:36 +0530

    created Nav

--- stray/src/components/Nav.jsx
+++ stray/src/components/Nav.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Nav = () => {
   return (
-    <nav className='flex items-center justify-around gap-4'>
-      <h4 className='font-poppins font-bold text-6xl'>stray.</h4>
-      
-      <Link>Men</Link>
-      <Link>Women</Link>
-      <Link>New-Arrivals</Link>
-      <Link>Help</Link>
-      <Link>Cart</Link>
-
-    
+    <nav 
+    className="flex items-center
+               justify-between gap-4 px-10 py-2">
+      <h4
+        className="font-poppins font-bold text-5xl italic"
+      >
+        stray. </h4>
+      <div
+        className="flex gap-5 pr-4 font-semibold 
+                   font-opensans tracking-wider text-lg"
+      >
+        <Link>Men</Link>
+        <Link>Women</Link>
+        <Link>New-Arrivals</Link>
+        <Link>Help</Link>
+        <Link>Cart</Link>
+      </div>
     </nav>
-  )
+  );
 };
