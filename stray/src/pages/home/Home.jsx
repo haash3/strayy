6ba0bf753e915aa366d180b4c75148e119e94dc6
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-04-01 13:19:18 +0530

    NewsLetter created and modified

--- stray/src/pages/home/Home.jsx
+++ stray/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Hero} from './components/Hero'
 import { Featured } from './components/Featured'
 import { Brands } from './components/Brands'
+import { NewsLetter } from './components/NewsLetter'
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
     <Hero />
     <Featured />
     <Brands/>
+    <NewsLetter />
     </>
   )
 }
---
+++ stray/src/pages/home/components/NewsLetter.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Envelope } from "@phosphor-icons/react";
+
+export const NewsLetter = () => {
+  return (
+    <section
+      className="text-center 
+                justify-center items-center gap-8 text-white
+                padding-x w-full pt-10 pb-10 bg-black/90 "
+    >
+      <div className="font-poppins">
+        <h1 className="font-dmsans text-[32px] font-bold pt-4 pb-2 flex justify-center items-center gap-2">
+          Newsletter <Envelope size={42} />
+        </h1>
+        <p className="text-lg pb-4 text-white/50">
+          Unlock early access and enjoy discounted rates with our subscription
+        </p>
+      </div>
+      <form className="py-4">
+        <input type="email" placeholder="your@email.com" 
+               className="p-2 m-2 w-[250px] text-center
+                         border-none outline-none rounded-sm
+                         text-black" />
+        <button type="submit" 
+                className="bg-gray-300 text-black p-2 mx-2 w-[100px] rounded-sm">
+          Subscribe
+        </button>
+      </form>
+    </section>
+  );
+};
