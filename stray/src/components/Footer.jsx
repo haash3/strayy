469909cbfec26a7f1efeb1704ee91b6d748c83bd
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-04-01 14:11:08 +0530

    Footer modified

--- stray/src/components/Footer.jsx
+++ stray/src/components/Footer.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import React from "react";
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-10 bg-black/90
+                       text-white/95 font-dmsans 
+                       lg:text-[18px] text-[16px]">
+        <div className="flex gap-6 justify-center py-2">
+            <p>About</p>
+            <p>Store locator</p>
+            <p>FAQs</p>
+            <p>News</p>
+            <p>Careers</p>
+            <p>Contact Us</p>
+        </div>
+        <p className="text-center py-2">Design by haash03</p>
+    </footer>
+  );
+};
