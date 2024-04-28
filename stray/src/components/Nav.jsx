e5fdf6dbbdeb9daee15a29b1dfc54c05ba70f3df
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-04-28 10:12:09 +0530

    navbar color change

--- stray/src/components/Nav.jsx
+++ stray/src/components/Nav.jsx
@@ -18,7 +18,7 @@ export const Nav = () => {
   return (
     <>
       <div
-        className=" flex items-center justify-between
+        className=" flex items-center justify-between 
                       padding-x py-4 text-center nav relative"
       >
         <Link to="/">
