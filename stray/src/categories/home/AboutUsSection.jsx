a76cabfdb9ab6ef6b0084624a70dd1d1c69ef54a
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-05-12 22:13:30 +0530

    about us section complete

--- stray/src/categories/home/AboutUsSection.jsx
+++ stray/src/categories/home/AboutUsSection.jsx
@@ -5,20 +5,18 @@ import {
   GlobeHemisphereWest,
   ArrowUpRight,
 } from "@phosphor-icons/react";
+import './aboutussection.css'
 
 export const AboutUsSection = () => {
   return (
     <>
-      <div className="padding-x py-[4rem] display">
-        <div
-          className="font-poppins lg:text-[2.25rem] text-[2rem]
-                                text-pantone font-semibold"
-        >
+      <div className="container">
+      <div className="section-head">
           About Us. &nbsp;
-          <span className="text-gray-500">Join Our Purpose</span>
+          <span className="text-gray-500">Join Our Journey</span>
         </div>
-        <div className="about-us ">
-          <div className="bg-aboutGrid relative img-grid rounded-[50px]">
+        <div className="about-us-section">
+          <div className="about-img-side">
             <Link to="/about">
               {/* <div className="learn-more-btn uppercase">
                 <p className="font-dmsans">Learn More </p>
@@ -26,39 +24,30 @@ export const AboutUsSection = () => {
               </div> */}
             </Link>
           </div>
-          <div className="aboutUs-text-grid rounded-[50px]">
-            <div
-              className="font-oswald text-[4rem] font-bold 
-                           tracking-wider leading-[6rem] uppercase"
-            >
-              We're Changing the way things get made.
-            </div>
-            <div className="aboutUs-text-box">
+          <div className="about-text-side">
+            <h1>We're Changing the way things get made.</h1>
+            <div className="about-text-side-info">
               <div>
-                <h2
-                  className="font-oswald uppercase text-[1.5rem] font-bold
-                                mb-2 flex items-center gap-1"
-                >
-                  <Planet size={34} className="grid-icon" />
-                  <p className="tracking-wide">Sustainability</p>
+                <h2>
+                  <Planet size={34} />
+                  <p>Sustainability</p>
                 </h2>
-                <p className="font-dmsans font-bold text-left">
+                <p className="description">
                   We're challenging conventional retail putting an end to dead
                   stock, unconventional waste and more.
                 </p>
               </div>
               <div>
-                <h2
-                  className="font-oswald uppercase text-[1.5rem] font-bold
-                                mb-2 flex items-center gap-1"
-                >
-                  <GlobeHemisphereWest size={34} className="grid-icon" />
-                  <p className="tracking-wide">Mission</p>
+                <h2>
+                  <GlobeHemisphereWest size={34}/>
+                  <p>Mission</p>
                 </h2>
-                <p className="font-dmsans font-bold text-left">
+                <p className="description">
                   We're on a mission to empower create independence in a
                   commercial world and incredible.
                 </p>
+                <button id="about-us-mini">Learn More  <ArrowUpRight size={30} /></button>
+
               </div>
             </div>
           </div>
